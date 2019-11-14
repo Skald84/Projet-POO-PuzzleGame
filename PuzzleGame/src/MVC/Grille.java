50172201a0a288029aa454215f16d10113aceacc
Ferrand <gerome.ferrand@gmail.com>
2019-11-14 17:08:36 +0100

    first Commit
    blabla

---
+++ PuzzleGame/src/MVC/Grille.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+/**
+ *
+ * @author et8ge
+ */
+public class Grille {
+    boolean resolu;
+    int longueur, largeur;
+    public static Case[][] Grille1 = [0,0,0,0,0,0,0];
+    
+    
+    public Grille(int lo, int la){
+        Case[][] Grille;
+        for(int i =0 ; i < lo ; i++){
+            for(int j =0 ; j < la ; j++){
+                Case[i][j] = new Case...();
+            }
+        }
+        
+    }
+}
