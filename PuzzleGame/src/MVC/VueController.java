49005370ee0466a7c509aa541b90bad7c6a5c5a2
Ferrand <gerome.ferrand@gmail.com>
2019-11-20 10:34:07 +0100

    commentaires/questionnements
    commentaire de questionnement sur le code

--- PuzzleGame/src/MVC/VueController.java
+++ PuzzleGame/src/MVC/VueController.java
@@ -99,17 +99,19 @@ public class VueController extends Application {
         BorderPane.setAlignment(grille, Pos.CENTER);
         //grille.setPrefSize(300,300);
         
+        
+        
         // fixe les dimensions du tableau en fonction de l'objet statique Grille (possibilité de fusionner avec déclaration suivante)
         int longueurGrille = Grille.lo, largeurGrille = Grille.la;
         
         // création d'un tableau de conteneur d'image
-        ImageView[][] tabImageView = new ImageView[longueurGrille][largeurGrille];
+        ImageView[][] tabImageView = new ImageView[longueurGrille][largeurGrille]; // EST-CE QUE CA NE SERAI PAS MIEUX DE FAIRE UN TABLEAU DE BOUTON[][]? OU CHAQUE BOUTON CONTIENT UNE IMG ?
         
         // insertion des images dans le tableau de conteneur d'images + listener Drag & Drop sur chaque conteneur
             for (int column = 0; column < longueurGrille; column++) {
                 for (int row = 0; row < largeurGrille; row++) {
 
-                    final int fColumn = column;
+                    final int fColumn = column;//FINAL DANS UNE BOUCLE ??
                     final int fRow = row;
 
                     ImageView imageView;
@@ -119,7 +121,7 @@ public class VueController extends Application {
                         // image symbole en début et fin de tableau
                             //création d'un conteneur d'img + ajout image dedans
                             imageView = new ImageView(new Image(VueController.class.getResourceAsStream("/images/S1.png")));
-                            Pane root = new Pane();
+                            Pane root = new Pane();//UN NOUVEAU PANEL ?
                             root.getChildren().add(imageView);
                             tabImageView[column][row] = imageView;
                             grille.add(tabImageView[column][row], column, row);
@@ -163,6 +165,7 @@ public class VueController extends Application {
                             public void handle(DragEvent event) {
 
                                 // attention, le setOnDragDone est dÃ©clenchÃ© par la source du Drag&Drop
+                                //|->??
 
                                 m.stopDD(fColumn, fRow);
                             }
